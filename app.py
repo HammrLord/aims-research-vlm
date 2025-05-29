9a844017f4b429a6b5c2a9ade5ec7af51a0d9223
Kartik Sharma <kartik12e4@gmail.com>
2025-05-29 23:31:34 +0530

    Added streamlit app and choice of number of examples.

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+import time
+import streamlit as st
+from PIL import Image
+from main import generate_summary
+from dotenv import load_dotenv
+
+st.title("🍽️Cook My Image")
+
+def stream_data(text):
+    for word in text.split(" "):
+        yield word + " "
+        time.sleep(0.05)
+
+if st.button("Reset Session",help="Reset this conversation.",type="primary"):
+    st.session_state.clear()
+    st.rerun()
+
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+UPLOAD_DIR_PERMANENT = "uploaded_images"
+os.makedirs(UPLOAD_DIR_PERMANENT, exist_ok=True)
+if uploaded_image:
+    image = Image.open(uploaded_image)
+    saved_image_path = os.path.join(UPLOAD_DIR_PERMANENT, uploaded_image.name)
+    image_bytes = uploaded_image.read()
+    try:
+        with open(saved_image_path, "wb") as f:
+            f.write(image_bytes)
+    except Exception as e:
+        st.error(f"An error occurred while saving the image: {e}")
+        st.write("Please ensure the uploaded file is a valid image.")
+
+    st.image(image, caption=uploaded_image.name)
+    noisy_title =  st.text_input("Outcast's Name")
+    if noisy_title:
+        response = generate_summary(test_image_path=saved_image_path,
+                                    test_title=noisy_title,
+                                    json_path="train/train.json",
+                                    img_dir="train",
+                                    API_KEY=os.getenv("GOOGLE_API_KEY"))
+        st.write_stream(stream_data(response))
+
+if __name__ == "__main__":
+    load_dotenv()
--- main.py
+++ main.py
@@ -12,22 +12,24 @@ def encode_image_base64(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode("utf-8")
 
-def load_all_examples(json_path, img_dir):
+def load_all_examples(json_path, img_dir,num_examples=5):
     with open(json_path, "r") as f:
         data = json.load(f)
-
-    examples = []
-    for item in data:
-        img_path = os.path.join(img_dir, item["image"])
-        image_b64 = encode_image_base64(img_path)
-        examples.append({
-            "title": item["image"],
-            "summary": item["recipe_summary"],
-            "image_b64": image_b64
-        })
+    if num_examples <= 0 and num_examples>14:
+        raise ValueError("num_examples must be a positive integer.")
+    else:
+        examples = []
+        for item in data[:num_examples]:
+            img_path = os.path.join(img_dir, item["image"])
+            image_b64 = encode_image_base64(img_path)
+            examples.append({
+                "title": item["image"],
+                "summary": item["recipe_summary"],
+                "image_b64": image_b64
+            })
     return examples
 
-def generate_summary(test_image_path, test_title, json_path, img_dir):
+def generate_summary(test_image_path, test_title, json_path, img_dir,API_KEY):
     examples = load_all_examples(json_path, img_dir)
     test_image_b64 = encode_image_base64(test_image_path)
 
@@ -35,7 +37,7 @@ def generate_summary(test_image_path, test_title, json_path, img_dir):
         {
             "type": "text",
             "text": (
-                """You are a chef's assistant that summarizes recipes in 3 lines or fewer.Below are some examples with\
+                """You are a chef's assistant that summarizes recipes in 3 lines. Below are some examples with\
                     a dish noisy title, an image, and a short recipe. Analyze the examples and then write a similar summary\
                     for the test image."""
             )
@@ -50,36 +52,21 @@ def generate_summary(test_image_path, test_title, json_path, img_dir):
     content.append({"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{test_image_b64}"}})
     content.append({"type": "text", "text": "Summary:"})
 
-    llm = GoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.7, google_api_key=os.getenv("GOOGLE_API_KEY"))
+    llm = GoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.7, google_api_key=API_KEY)
     response = llm.invoke([HumanMessage(content=content)])
-
-    print("\nGenerated Recipe Summary:")
     return response
 
 if __name__ == "__main__":
     with open("test/test.json", "r") as f:
         test_data = json.load(f)
-    image_path = os.path.join("test",test_data[4]["image"])
+    test_number = 4
+    image_path = os.path.join("test",test_data[test_number]["image"])
     logs = generate_summary(
             test_image_path= image_path,
-            test_title=test_data[4]["image"],
+            test_title=test_data[test_number]["image"],
             json_path="train/train.json",
             img_dir="train"
         )
     with open("log.txt","a") as file:
-        file.write(f"Image: {test_data[4]['image']}\n")
+        file.write(f"Image: {test_data[test_number]['image']}\n")
         file.write(f"Summary: {logs}\n\n")
-    # counter = 0
-    # with open("test/test.json", "r") as f:
-    #     test_data = json.load(f)
-    # for i in test_data:
-    #     print(f"⚙Testing Image: {counter}\n")
-    #     image_path = os.path.join("test",i["image"])
-    #     generate_summary(
-    #         test_image_path= image_path,
-    #         test_title=i["image"],
-    #         json_path="train/train.json",
-    #         img_dir="train"
-    #     )
-    #     counter += 1
-    #     time.sleep(4)
